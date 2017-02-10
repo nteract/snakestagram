6425070879ec39744fc29cc49264c443738a440f
Min RK <benjaminrk@gmail.com>
2017-02-10 11:18:31 +0100

    put build/install.sh in unix-common
    use environment variables for tiny difference between mac, linux

--- osx/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-conda create -p /tmp/snakestagram --copy -y -q python=3 pandas scikit-learn ipykernel
-
-cd /tmp/
-tar czf snakestagram-${VERSION}-darwin-x64.tar.gz ./snakestagram
-ls .
